3808cd2ce7afb4f396b9facfac0725db668fbfc4
Technologist 036 <120473606+Technologist0366@users.noreply.github.com>
2025-07-29 20:02:58 -0400

    Update answers.sql

--- database/assign4/answers.sql
+++ database/assign4/answers.sql
@@ -1,1 +1,44 @@
+Question 1: Total payment amount per date (Top 5 latest)
 
+SELECT 
+    paymentDate, 
+    SUM(amount) AS total_amount
+FROM 
+    payments
+GROUP BY 
+    paymentDate
+ORDER BY 
+    paymentDate DESC
+LIMIT 5;
+
+Question 2: Average credit limit per customer (grouped by name and country)
+
+SELECT 
+    customerName, 
+    country, 
+    AVG(creditLimit) AS average_credit_limit
+FROM 
+    customers
+GROUP BY 
+    customerName, country;
+
+Question 3: Total price per product ordered
+
+SELECT 
+    productCode, 
+    quantityOrdered, 
+    (quantityOrdered * priceEach) AS total_price
+FROM 
+    orderdetails
+GROUP BY 
+    productCode, quantityOrdered, priceEach;
+
+Question 4: Highest payment amount per check number
+
+SELECT 
+    checkNumber, 
+    MAX(amount) AS highest_amount
+FROM 
+    payments
+GROUP BY 
+    checkNumber;
