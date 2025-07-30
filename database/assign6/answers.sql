c0a33482d1a5ee742de513c81311e53cf4a22cf1
Technologist 036 <120473606+Technologist0366@users.noreply.github.com>
2025-07-30 04:20:42 -0400

    Update answers.sql

--- database/assign6/answers.sql
+++ database/assign6/answers.sql
@@ -1,1 +1,42 @@
 
+Question 1: INNER JOIN – employees and offices
+
+SELECT 
+    employees.firstName, 
+    employees.lastName, 
+    employees.email, 
+    employees.officeCode
+FROM 
+    employees
+INNER JOIN 
+    offices 
+ON 
+    employees.officeCode = offices.officeCode;
+
+Question 2: LEFT JOIN – products and productlines
+
+SELECT 
+    products.productName, 
+    products.productVendor, 
+    products.productLine
+FROM 
+    products
+LEFT JOIN 
+    productlines 
+ON 
+    products.productLine = productlines.productLine;
+
+Question 3: RIGHT JOIN – customers and orders
+
+SELECT 
+    orders.orderDate, 
+    orders.shippedDate, 
+    orders.status, 
+    orders.customerNumber
+FROM 
+    customers
+RIGHT JOIN 
+    orders 
+ON 
+    customers.customerNumber = orders.customerNumber
+LIMIT 10;
